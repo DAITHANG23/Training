44502bcdb2c3d6fa13cb0ddec4bf887b766ed896
DAITHANG23 <nguyendaithang23061997@gmail.com>
2023-08-26 15:44:46 +0700

    add lesson 07

---
+++ lesson7/login.js
@@ -0,0 +1,48 @@
+/*
+{
+  dsadsad;
+}
+
+*/
+// 5 kiểu dữ liệu nguyên thủy (primitive) : number, string , boolean, undefine, null
+
+// Dữ liệu number
+let x = 10; // x =10
+x = 12;
+
+const y = 12;
+//y = 13;
+console.log("giá trị của y", y);
+
+console.log("Giá trị x + y:", x + y);
+const a = 13;
+
+// Dữ liệu string
+
+const name = "Nguyen Dai Thang";
+console.log("tên là :", name);
+
+const nameCountry = "Viet Nam";
+
+let b = 1;
+console.log("giá trị của b = ", b);
+console.log("gia tri của x:", x);
+
+// kiểu dữ liệu Boolean
+const value = true; // true = 1; false =0
+
+const abc = null;
+
+console.log("Giá trị x + value:", x + value);
+console.log("Giá trị x + abc:", x + abc);
+//  dữ liễu undefine xảy ra khi kết quả mình mong muốn ko dc xuất ra
+
+const xyz = "abcdef";
+asd = xyz;
+
+console.log("giá tri asd:", asd);
+
+const value1 = 10;
+const value2 = 20;
+let value3 = value3 + value1;
+value3 += value1;
