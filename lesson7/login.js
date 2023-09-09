9d2320d66fe9a293b8cbdad1007210ec64f990d8
DAITHANG23 <nguyendaithang23061997@gmail.com>
2023-09-09 15:25:59 +0700

    update

--- lesson7/login.js
+++ lesson7/login.js
@@ -7,42 +7,78 @@
 // 5 kiểu dữ liệu nguyên thủy (primitive) : number, string , boolean, undefine, null
 
 // Dữ liệu number
-let x = 10; // x =10
-x = 12;
+// let x = 10; // x =10
+// x = 12;
 
-const y = 12;
-//y = 13;
-console.log("giá trị của y", y);
+// const y = 12;
+// //y = 13;
+// console.log("giá trị của y", y);
 
-console.log("Giá trị x + y:", x + y);
-const a = 13;
+// console.log("Giá trị x + y:", x + y);
+// const a = 13;
+// console.log(a);
+// // Dữ liệu string
 
-// Dữ liệu string
+// let name = "Nguyen Dai Thang";
+// console.log("tên là :", name);
 
-const name = "Nguyen Dai Thang";
-console.log("tên là :", name);
+// const nameCountry = "Viet Nam";
 
-const nameCountry = "Viet Nam";
+// let b = 1;
+// console.log("giá trị của b = ", b);
+// console.log("gia tri của x:", x);
 
-let b = 1;
-console.log("giá trị của b = ", b);
-console.log("gia tri của x:", x);
+// // kiểu dữ liệu Boolean
+// const value = true; // true = 1; false =0
 
-// kiểu dữ liệu Boolean
-const value = true; // true = 1; false =0
+// const abc = null;
 
-const abc = null;
+// console.log("Giá trị x + value:", x + value);
+// console.log("Giá trị x + abc:", x + abc);
+// //  dữ liễu undefine xảy ra khi kết quả mình mong muốn ko dc xuất ra
 
-console.log("Giá trị x + value:", x + value);
-console.log("Giá trị x + abc:", x + abc);
-//  dữ liễu undefine xảy ra khi kết quả mình mong muốn ko dc xuất ra
+// let abcde = 10;
+// console.log("giá trị abcde:", abcde);
+// const xyz = "abcdef";
+// asd = xyz;
 
-const xyz = "abcdef";
-asd = xyz;
+// console.log("giá tri asd:", asd);
 
-console.log("giá tri asd:", asd);
+// const value1 = 10;
+// const value2 = 20;
+// let value3 = value3 + value1;
+// value3 += value1;
 
-const value1 = 10;
-const value2 = 20;
-let value3 = value3 + value1;
-value3 += value1;
+// {
+//   const name  = 10;
+// }
+
+let a = 12;
+const b = 10;
+let c = 0;
+c += a; //c=c+a = 12
+console.log("c:", c);
+c -= a; //c=c-a
+console.log("c:", c);
+console.log("a+b:", a + b);
+console.log("a-b:", a - b);
+console.log("a*b:", a * b);
+console.log("a/b:", a / b);
+console.log("a%b:", a % b);
+console.log("a**b:", a ** b);
+a++;
+a--;
+console.log("a++:", a);
+
+console.log("a--:", a);
+const d = a;
+console.log("giá trị d:", d);
+
+const x = "1";
+const y = "1";
+console.log("so sánh ==:", x == y); // ra kết quả là boolean
+console.log("so sánh ===:", x === y);
+
+// && : toán phép nhân (và)
+// || : toán cộng (hoặc)
+// ! : phủ định (not)
